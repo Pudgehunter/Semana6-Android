445a82d70ea1d4aa925e01ec1feef2e9ca290cfe
Yoshiki Tsutsui <pudgehunter2@hotmail.com>
2020-10-02 00:40:35 -0500

    Terminado

---
+++ app/src/main/java/co/domi/semana6/Bienvenido.java
@@ -0,0 +1,16 @@
+package co.domi.semana6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bienvenido extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenido);
+    }
+}
---
+++ app/src/main/java/co/domi/semana6/model/User.java
@@ -0,0 +1,32 @@
+package co.domi.semana6.model;
+
+public class User {
+    String usuario;
+    String password;
+    String description;
+
+
+    public User(String usuario, String password, String description) {
+        this.usuario = usuario;
+        this.password = password;
+        this.description = description;
+    }
+
+    //Necesario para Gson
+    public User() {
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
